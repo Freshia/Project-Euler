bd1a42bd954bd489a090a6548b9fe7a75fbf4a0b
Freshia <freshiasackey@gmail.com>
2020-03-06 11:47:01 +0300

    Added problem16 solution

---
+++ problem16.scala
@@ -0,0 +1,18 @@
+import scala.math._
+import collection.immutable.IndexedSeq
+object problem16{
+    def main(Args: Array[String]){
+        val power:Int = pow(2,1000).toInt;
+        var powerString= power.toString;
+        println(powerString)
+        var sum:Int = 0;
+        //sum = powerString.map(_.toInt+sum);
+        for(i<-0 until powerString.length){
+            var c = powerString.charAt(i);
+            println(c)
+            sum = sum + c.toInt;
+        }
+        print(sum);
+
+    }
+}
