6d7585b7d8c8847760fa4054487c5a864bf675d2
Freshia <freshiasackey@gmail.com>
2020-03-18 16:17:12 +0300

    Changed solution to use BigDecimal

--- problem16.scala
+++ problem16.scala
@@ -5,8 +5,8 @@ object problem16{
     def main(Args: Array[String]){
         val power:Double = pow(2,1000);
         println(power);
-        var powerString="%.0f".format(power);
-        
+        //var powerString="%.0f".format(power);
+        var powerString = new BigDecimal(power).toPlainString();
         var sum:Int = 0;
         for(i<-0 until powerString.length){
             var c = powerString.charAt(i).asDigit;
