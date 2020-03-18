4155452fe4aa1544eefac7e2576e45c285f25822
Freshia <freshiasackey@gmail.com>
2020-03-18 16:15:24 +0300

    Solves issue #1

--- problem16.scala
+++ problem16.scala
@@ -1,16 +1,20 @@
 import scala.math._
+import java.math.BigDecimal;
 import collection.immutable.IndexedSeq
 object problem16{
     def main(Args: Array[String]){
-        val power = pow(2,1000);
-        var powerString= power.toString;
-        println(powerString)
+        val power:Double = pow(2,1000);
+        println(power);
+        var powerString="%.0f".format(power);
+        
         var sum:Int = 0;
-        //sum = powerString.map(_.toInt+sum);
         for(i<-0 until powerString.length){
-            var c = powerString.charAt(i);
-            println(c)
-            sum = sum + c.toInt;
+            var c = powerString.charAt(i).asDigit;
+            if(c!=0){
+                println(c);
+                sum = sum + c;
+            }
+            
         }
         print(sum);
 
